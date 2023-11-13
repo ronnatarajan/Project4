49d96b1558ecb0ffe064c693a2d145d084fc7215
Kenjie DeCastro <143752784+KenjieD-11@users.noreply.github.com>
2023-11-13 12:57:13 -0500

    Update InvalidMessageException.java

--- src/InvalidMessageException.java
+++ src/InvalidMessageException.java
@@ -1,5 +1,15 @@
 package src;
 
+/**
+ * Project 4 -- Messaging System
+ *
+ *  CLASS DESCRIPTION
+ *
+ * @author NAME, lab sec 23
+ *
+ * @version November 13, 2023
+ */
+
 public class InvalidMessageException extends Exception {
     public InvalidMessageException(String message) {
         super(message);
