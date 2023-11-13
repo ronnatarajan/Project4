eec7c0a6d4d8d0c290c97bba57dbe554601b3f81
Kenjie DeCastro <143752784+KenjieD-11@users.noreply.github.com>
2023-11-12 22:22:48 -0600

    Added a test case class for SendMessages.java and Stores.java

---
+++ src/StoresTestCase.java
@@ -0,0 +1,60 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StoresTestCase {
+    public static void main(String[] args) {
+
+        // Test Data
+        String stores = "Bob's Hardware,Bob's Furniture";
+        String sellerEmail = "Bob@gmail.com";
+        appendStoresTestCase(stores, sellerEmail);
+
+
+        // Error Test Data
+        stores = "";
+        System.out.println("\n\nERROR TEST DATA:");
+        appendStoresTestCase(stores, sellerEmail);
+    }
+
+    public static void appendStoresTestCase(String stores, String sellerEmail) {
+        System.out.println("appendStores Test Case");
+
+
+
+        Stores.appendStores(stores, sellerEmail);
+
+        boolean stringFoundStore = false;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("Database/Lists/StoresList.txt"));
+
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.equals(sellerEmail + ":" + stores)) {
+                    stringFoundStore = true;
+                } else {
+                    stringFoundStore = false;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (stringFoundStore) {
+            System.out.println("Test Passed: The store was written to the StoresList.txt file. Did not crash.");
+        } else {
+            System.out.println("Test Failed: The store was not written to the StoresList.txt file");
+        }
+
+        System.out.println("\nRemember to delete the following lines in the StoresList.txt file and leave an extra empty line!" +
+                "\n" + sellerEmail + ":" + stores);
+    }
+}
