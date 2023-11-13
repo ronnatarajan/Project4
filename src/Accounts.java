2722d9cfce34273257c9b085fc4d5b9fdf5eeffe
Kenjie DeCastro <143752784+KenjieD-11@users.noreply.github.com>
2023-11-13 12:55:07 -0500

    Update Accounts.java

--- src/Accounts.java
+++ src/Accounts.java
@@ -2,8 +2,21 @@ package src;
 
 import java.io.*;
 
+/**
+ * Project 4 -- Messaging System
+ *
+ *  CLASS DESCRIPTION
+ *
+ * @author NAME, lab sec 23
+ *
+ * @version November 13, 2023
+ */
+
 public class Accounts {
 
+
+    // checks if the passed in email and password arguments are within
+    // the file listing all created accounts, therefore verifying if an account exits
     public static String checkAccount(String email, String password, String path) {
 
         try {
@@ -13,12 +26,12 @@ public class Accounts {
             boolean found = false;
             String line = bufferedReader.readLine();
             while (line != null) {
+                // Turns the read line into a String Array to help parse the email and password
+                // of the account
                 String[] lineArr = line.split(",");
 
                 if ((email.equals(lineArr[0])) && (password.equals(lineArr[1]))) {
                     return "Found account";
-                    // return "Customer account found!";
-                    // for testing
                 } else if ((email.equals(lineArr[0]))){
                     return "Found account";
                 }
@@ -35,28 +48,28 @@ public class Accounts {
         }
 
         return "";
-
-//        return "Customer account not found!";
-//        for testing
     }
 
 
-
+    // adds a customer account to the Customer Accounts list and creates an individual file
+    // for the customer where its messages are stored
     public static String addCustomerAccount(String customerEmail, String customerPassword) {
+        // For a customer trying to sign up, checks to see if the account has already been created before
         System.out.println(checkAccount(customerEmail, customerPassword, "Database/Lists/CustomerAccountsList.txt"));
-        if (checkAccount(customerEmail, customerPassword, "Database/Lists/SellerAccountsList.txt").equals("Found account") || checkAccount(customerEmail, customerPassword, "Database/Lists/CustomerAccountsList.txt").equals("Found account")) {
+        if (checkAccount(customerEmail, customerPassword, "Database/Lists/SellerAccountsList.txt").equals("Found account")
+                || checkAccount(customerEmail, customerPassword, "Database/Lists/CustomerAccountsList.txt").equals("Found account")) {
             System.out.println("Customer account already exists!");
             return "Customer account already exists";
         }
 
+        // Writes the account in the specified format to the Customer Accounts list file
+        // Creates accounts own individual text file
         try {
             PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("Database/Lists/CustomerAccountsList.txt", true)));
             printWriter.println(customerEmail + "," + customerPassword + "," + "[]" + "," + "[]");
 
             File f = new File("Accounts/" + customerEmail + ".txt");
             f.createNewFile();
-
-            System.out.println("came here");
 
             printWriter.flush();
             printWriter.close();
@@ -68,12 +81,18 @@ public class Accounts {
         return "Customer account can be created";
     }
 
+    // adds a seller account to Seller Accounts list and creates an individual file
+    // for the seller where its messages are stored
     public static String addSellerAccount(String sellerEmail, String sellerPassword) {
-        if (checkAccount(sellerEmail, sellerPassword, "Database/Lists/SellerAccountsList.txt").equals("Found account") || checkAccount(sellerEmail, sellerPassword, "Database/Lists/CustomerAccountsList.txt").equals("Found account")) {
+        // For a seller trying to sign up, checks to see if the account has already been created before
+        if (checkAccount(sellerEmail, sellerPassword, "Database/Lists/SellerAccountsList.txt").equals("Found account")
+                || checkAccount(sellerEmail, sellerPassword, "Database/Lists/CustomerAccountsList.txt").equals("Found account")) {
             System.out.println("Seller account already exists!");
             return "Seller account already exists!";
         }
 
+        // Writes the account in the specified format to the Seller Accounts list file
+        // Creates accounts own individual text file
         try {
             PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("Database/Lists/SellerAccountsList.txt", true)));
             printWriter.println(sellerEmail + "," + sellerPassword + "," + "[]" + "," + "[]");
@@ -89,40 +108,6 @@ public class Accounts {
             System.out.println("SellerAccountsList.txt does not exist!");
         }
         return "Seller account can be created";
-    }
-    public static void main(String[] args) {
-
-        // Test checkAccount method
-        System.out.println("Testing checkAccount method:");
-        String email = "test@example.com";
-        String password = "password123";
-        String path = "Database/Lists/CustomerAccountsList.txt";
-        String result = Accounts.checkAccount(email, password, path);
-        System.out.println(result);
-        System.out.println();
-
-        // Test addCustomerAccount method
-        System.out.println("Testing addCustomerAccount method:");
-        String newCustomerEmail = "newcustomer@example.com";
-        String newCustomerPassword = "newpassword456";
-        result = Accounts.addCustomerAccount(newCustomerEmail, newCustomerPassword);
-        System.out.println(result);
-        System.out.println();
-
-        // Test addSellerAccount method
-        System.out.println("Testing addSellerAccount method:");
-        String newSellerEmail = "newseller@example.com";
-        String newSellerPassword = "sellerpassword789";
-        result = Accounts.addSellerAccount(newSellerEmail, newSellerPassword);
-        System.out.println(result);
-        System.out.println();
-
-        // Test checkAccount after adding new accounts
-        System.out.println("Testing checkAccount after adding accounts:");
-        result = Accounts.checkAccount(newCustomerEmail, newCustomerPassword, path);
-        System.out.println(result);
-        result = Accounts.checkAccount(newSellerEmail, newSellerPassword, path);
-        System.out.println(result);
     }
 }
 
